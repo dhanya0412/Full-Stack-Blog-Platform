7afbfc5769b3e5be2b478430fd265e3803ac3acb
dhanya0412 <dhanyagirdhar@gmail.com>
2025-07-21 00:35:08 +0530

    integrated mongo atlas

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ const passport = require('passport');
 const initializePassport = require('./config/passport');
 const blogRoutes = require('./routes/blog');
 const port = 3000;
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost:27017/blog-app', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_URL, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('MongoDB Atlas connection error:', err));
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
